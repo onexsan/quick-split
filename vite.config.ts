676901689214a7e9aa120572ee3dbf15600e41fd
Victoria <v.filileeva@seowork.ru>
2023-03-01 19:36:11 +0400

    Added base for a deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quick-split/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
